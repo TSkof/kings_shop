f340ae0219ba1a48f700dfdb2dfb87cc8833ab9e
Tomislav Skof <tomislav.skof@hotmail.de>
2022-01-18 21:36:23 +0100

    routing, collectio, header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import './App.css';
-
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+
+import './App.css';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+
 
 function App() {
   return (
-    <div >
-      <HomePage />
+    <div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
+    </BrowserRouter>
     </div>
   );
 }
