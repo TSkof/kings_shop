ca4d5b0536b99a7902e13cabd118f91411a1432e
Tomislav Skof <tomislav.skof@hotmail.de>
2022-01-22 12:56:04 +0100

    firebase utils, database, register

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,27 @@ const config = {
   appId: "1:903113507961:web:fde83acb8c4492998e40a8"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) =>  {
+  if(!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  if(!snapShot.exists){
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try{
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    }catch (error){
+      console.log('error creating user', error.message);
+    }
+  }
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
