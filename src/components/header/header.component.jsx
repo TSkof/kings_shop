1acf478c3f6dff830d06ca8c866b43f4be04e791
Tomislav Skof <tomislav.skof@hotmail.de>
2022-01-22 17:55:31 +0100

    redux + reducer + actions

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { ReactComponent as Logo } from "../../assets/king.svg";
+import { connect } from "react-redux";
+
 import { auth } from "../../firebase/firebase.utils";
+
+import { ReactComponent as Logo } from "../../assets/king.svg";
 import "./header.styles.scss";
 
 const Header = ({ currentUser }) => (
@@ -30,4 +33,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
